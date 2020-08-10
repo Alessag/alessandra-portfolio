cc358eda62c63b4d8b6b0aa6b93a170011d8d1b9
Alessandra <alessamica@gmail.com>
2020-08-09 22:14:48 -0400

    Footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import {
+  AiOutlineMail,
+  AiOutlineInstagram,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillHeart,
+  AiOutlineCopyrightCircle,
+} from 'react-icons/ai';
+import '../assets/styles/components/Footer.scss';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      <h1>I am a Footer</h1>
-    </div>
+    <Container id='footer'>
+      <Divider orientation='horizontal' />
+      <ul className='footer__social-links'>
+        <li>
+          <AiOutlineMail size='1.5rem' color='white' />
+        </li>
+        <li>
+          <AiOutlineInstagram size='1.3rem' color='white' />
+        </li>
+        <li>
+          <AiFillLinkedin size='1.3rem' color='white' />
+        </li>
+        <li>
+          <AiFillGithub size='1.3rem' color='white' />
+        </li>
+      </ul>
+      <h4 className='footer__text'>
+        Made with <AiFillHeart color='#54ecc4' /> in San Cristóbal, Venezuela
+      </h4>
+      <h4 className='footer__text'>
+        2020 <AiOutlineCopyrightCircle /> Alessandra Amicarella
+      </h4>
+    </Container>
   );
-}
+};
 
 export default Footer;
