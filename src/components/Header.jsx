30ffd27d0ae41aeb97adfddd059d8c48d85bde2b
Alessandra <alessamica@gmail.com>
2020-08-19 21:36:40 -0400

    Container and remove light theme navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
+  Container,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -14,25 +15,27 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
-    <div>
-      <Navbar color='light' light expand='md'>
-        <NavbarBrand href='/'>Alessandra</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className='mr-auto' navbar>
-            <NavItem>
-              <Link to='/'>Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/about'>About</Link>
-            </NavItem>
-            <NavItem>
-              <Link to='/contact'>Contact</Link>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+    <header className='header'>
+      <Container fluid>
+        <Navbar expand='md'>
+          <NavbarBrand href='/'>Alessandra</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar className='justify-content-end'>
+            <Nav navbar>
+              <NavItem>
+                <Link to='/'>Home</Link>
+              </NavItem>
+              <NavItem>
+                <Link to='/about'>About</Link>
+              </NavItem>
+              <NavItem>
+                <Link to='/contact'>Contact</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Container>
+    </header>
   );
 };
 
