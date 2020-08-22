aa63d766ad1420ac2727bbf57d139a1d5584725e
Alessandra <alessamica@gmail.com>
2020-08-21 23:31:58 -0400

    Bootstrap responsive menu removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../assets/styles/components/Header.scss';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  Container,
-} from 'reactstrap';
+import { Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
   return (
     <header className='header'>
-      <Container fluid>
-        <Navbar expand='md'>
-          <NavbarBrand href='/'>Alessandra</NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar className='justify-content-end'>
-            <Nav navbar>
-              <NavItem>
-                <Link to='/'>Home</Link>
-              </NavItem>
-              <NavItem>
-                <Link to='/about'>About</Link>
-              </NavItem>
-              <NavItem>
-                <Link to='/contact'>Contact</Link>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+      <Container fluid className='header-nav'>
+        <h4>Ale</h4>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
       </Container>
     </header>
   );
