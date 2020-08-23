0fb5bbf377a7a60292fbf05257b1508185c70332
Alessandra <alessamica@gmail.com>
2020-08-23 12:39:26 -0400

    Require image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,32 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
-import cover from '../assets/images/01.png';
 import '../assets/styles/components/Projects.scss';
 
-const Projects = ({ projects }) => (
-  <Container>
-    <h1 className='projects-content__title display-3'>Personal Projects</h1>
-    <Row className='justify-content-center'>
-      {projects.map((item) => (
-        <Col key={item.id} md='6' lg='4' className='mb-2'>
-          <img
-            src={cover}
-            className='img-fluid project-image'
-            alt={item.title}
-          />
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+const Projects = ({ projects }) => {
+  return (
+    <Container>
+      <h1 className='projects-content__title'>Personal Projects</h1>
+      <Row className='justify-content-center'>
+        {projects.map((item) => (
+          <Col key={item.id} md='6' lg='4' className='projects-content__card'>
+            <img
+              src={require(`../assets/images/projects/${item.img}`)}
+              className='img-fluid project-image'
+              alt={item.title}
+            />
+            <h3>{item.title}</h3>
+            {/* <p>{item.tecnologies}</p> */}
+            <a href={item.url}>Demo</a>
+            <a href={item.url}>Code</a>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
 Projects.propTypes = {
   projects: PropTypes.node.isRequired,
