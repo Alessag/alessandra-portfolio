a9bc43aac860aae6436ca630764fc75d5a705038
Alessandra <alessamica@gmail.com>
2020-08-23 14:58:45 -0400

    Props destructuring and check prop type

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Certificates from './Certificates';
+import Experience from './Experience';
 
-function About() {
+function About({ skills, certificates }) {
   return (
     <div>
-      <h1>I am a About me</h1>
+      <Experience skills={skills} />
+      <Certificates certificates={certificates} />
     </div>
   );
 }
+
+About.propTypes = {
+  skills: PropTypes.array.isRequired,
+  certificates: PropTypes.array.isRequired,
+};
 
 export default About;
