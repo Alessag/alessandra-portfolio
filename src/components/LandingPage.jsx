3792fdaa8fcdef198566450e3c4ab817b1238d3e
Alessandra <alessamica@gmail.com>
2020-08-27 15:51:52 -0400

    Home cube title container and styles

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container } from 'reactstrap';
 import Info from './Info';
 import Projects from './Projects';
+import '../assets/styles/components/LandingPage.scss';
 
 function LandingPage({ projects }) {
   return (
     <div>
+      <Container>
+        <div className='cube'>Front End Developer</div>
+      </Container>
       <Info />
       <Projects projects={projects} />
     </div>
