3d95051d790cb45a4ef98393cae9164e3628e472
Alessandra <alessamica@gmail.com>
2020-08-29 21:53:27 -0400

    Remove view projects links

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import { PrimaryButton } from './Button';
+import { Link } from 'react-router-dom';
 import AlePhoto from '../assets/images/alessandra.jpg';
 import '../assets/styles/components/Info.scss';
 
@@ -16,9 +16,7 @@ const Info = () => {
             digital world and discover new techs. Actually I am making inroads
             hunting in new projects with React.
           </p>
-          <PrimaryButton href='https://google.com'>View Projects</PrimaryButton>{' '}
-          or{' '}
-          <PrimaryButton href='https://google.com'>Read About Me</PrimaryButton>
+          <Link to='/about'>Read About Me</Link>
         </Col>
         <Col sm='12' md='6' className='info-container__img'>
           <img src={AlePhoto} className='img-fluid' alt='Alessandra' />
