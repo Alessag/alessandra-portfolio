c36be8f275adaa823832894ceaa1887c405d967b
Alessandra <alessamica@gmail.com>
2020-08-29 21:54:04 -0400

    Social network links

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,6 +8,46 @@ import '../assets/styles/components/LandingPage.scss';
 function LandingPage({ projects }) {
   return (
     <div className='home-page'>
+      <div className='social-container'>
+        <ul>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/AlessandraAmicarella/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://t.me/Alessamica'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Telegram
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://github.com/alessag'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://instagram.com/amicarellaa'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </div>
       <Container className='home-container'>
         <div className='cube'>Front End Developer</div>
       </Container>
