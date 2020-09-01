87c60c875e9df466f2fa2eea6e1b001cc4fb0f48
Alessandra <alessamica@gmail.com>
2020-09-01 11:35:10 -0400

    custom hook useData

---
+++ src/hooks/useData.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+const useData = (url) => {
+  const [person, setPerson] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((myData) => setPerson(myData));
+  }, []);
+
+  return person;
+};
+
+export default useData;
