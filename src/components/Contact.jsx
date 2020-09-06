e79f2aa0831d3315e7c7de69a26054b6f3fb67f3
Alessandra <alessamica@gmail.com>
2020-09-06 17:05:40 -0400

    Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Hey, I am an H1</h1>
+      <h2>Hey, I am an h2</h2>
+    </div>
+  );
+};
+
+export default Contact;
