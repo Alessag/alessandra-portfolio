e3171724a95781975096e1b03c3d9a2e0512bd7b
Alessandra <alessamica@gmail.com>
2020-09-09 11:44:07 -0400

    Imcorporate react hooks form library and register fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,42 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmitForm = (data) => {
+    return data;
+  };
+
   return (
     <div>
       <h1>Form contact</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmitForm)}>
         <label>Name</label>
-        <input type='text' name='name' />
+        <input
+          type='text'
+          name='name'
+          ref={register({ required: true, minLength: 3 })}
+        />
 
         <label>Email</label>
-        <input type='text' name='email' />
+        <input
+          type='text'
+          name='email'
+          ref={register({
+            required: true,
+            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+          })}
+        />
 
         <label>Message</label>
-        <textarea />
+        <textarea
+          type='text'
+          name='message'
+          ref={register({ required: true, minLength: 2 })}
+        />
         <input type='submit' value='Submit' />
       </form>
     </div>
