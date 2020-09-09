39dbefd09d90f56cc0aeca29b8d9931734545fc5
Alessandra <alessamica@gmail.com>
2020-09-09 11:29:01 -0400

    Initial form contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const Contact = () => {
   return (
     <div>
-      <h1>Hey, I am an H1</h1>
-      <h2>Hey, I am an h2</h2>
+      <h1>Form contact</h1>
+      <form>
+        <label>Name</label>
+        <input type='text' name='name' />
+
+        <label>Email</label>
+        <input type='text' name='email' />
+
+        <label>Message</label>
+        <textarea />
+        <input type='submit' value='Submit' />
+      </form>
     </div>
   );
 };
