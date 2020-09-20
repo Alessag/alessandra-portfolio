e43a03664aaf6f86a46a8c060762f71f337820aa
Alessandra <alessamica@gmail.com>
2020-09-20 15:35:06 -0400

    Remove social container links

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,46 +8,6 @@ import Contact from './Contact';
 function LandingPage({ projects, name, profession, bio }) {
   return (
     <div className='home-page'>
-      <div className='social-container'>
-        <ul>
-          <li>
-            <a
-              href='https://www.linkedin.com/in/AlessandraAmicarella/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Linkedin
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://t.me/Alessamica'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Telegram
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://github.com/alessag'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Github
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://instagram.com/amicarellaa'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Instagram
-            </a>
-          </li>
-        </ul>
-      </div>
       <Info name={name} profession={profession} bio={bio} />
       <Projects projects={projects} />
       <Contact />
