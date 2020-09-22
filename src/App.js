5f62f6facdd723b6aa1e7030354349d271661d88
Alessandra <alessamica@gmail.com>
2020-09-21 21:43:56 -0400

    Url cv props

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
         certificates={data.certifications}
         experience={data.experience}
         skills={data.skills}
+        urlCv={data.urlCv}
       />
       <Footer />
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import Certificates from './Certificates';
 import Experience from './Experience';
 
-function About({ skills, experience, certificates }) {
+function About({ skills, experience, certificates, urlCv }) {
   return (
     <div>
-      <Experience skills={skills} experience={experience} />
+      <Experience skills={skills} experience={experience} urlCv={urlCv} />
       <Certificates certificates={certificates} />
     </div>
   );
@@ -16,6 +16,7 @@ About.propTypes = {
   skills: PropTypes.array.isRequired,
   experience: PropTypes.array.isRequired,
   certificates: PropTypes.array.isRequired,
+  urlCv: PropTypes.string.isRequired,
 };
 
 export default About;
