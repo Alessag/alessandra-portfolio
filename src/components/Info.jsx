caf4a2c675f9e9758fb687c9d3dfc87879b0318a
Alessandra <alessamica@gmail.com>
2020-09-28 22:18:31 -0400

    Change ale photo and col size from info container text

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import AlePhoto from '../assets/images/ale.jpg';
+import AlePhoto from '../assets/images/alessandra.jpg';
 import '../assets/styles/components/Info.scss';
 
 const Info = ({ name, profession, bio }) => {
   return (
     <Container className='info-container'>
       <Row>
-        <Col sm='12' md='6' className='info-container__text'>
+        <Col sm='12' md='5' className='info-container__text'>
           <h6 className='sub-title'>Hi, I am {name}</h6>
           <h1 className='primary-title '>{profession} That Love Challenges</h1>
           <p className='text'>{bio}</p>
           <Link to='/about'>Read About Me</Link>
         </Col>
-        <Col sm='12' md='6' className='info-container__img'>
+        <Col sm='12' md='7' className='info-container__img'>
           <img src={AlePhoto} className='img-fluid' alt={name} />
         </Col>
       </Row>
