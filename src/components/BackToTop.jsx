abdbc1cf289a5ac388377bb8b5d26362b11f2845
Alessandra <alessamica@gmail.com>
2020-10-18 00:51:14 -0400

    Back to top component w styled components and emotion styles

---
+++ src/components/BackToTop.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from 'emotion';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import { RiArrowUpSLine } from 'react-icons/ri';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const gray = '#bcbcbc';
+const purple = '#8083ff';
+
+const BackToTop = () => {
+  return (
+    <Wrapper>
+      <Link
+        className={css`
+          color: ${gray};
+          border: 1.5px solid ${purple};
+          border-radius: 50%;
+          &:hover {
+            background-color: ${purple};
+            color: ${gray};
+          }
+        `}
+        activeClass='active'
+        to='top'
+        spy
+        smooth
+        offset={-70}
+        duration={500}
+      >
+        <RiArrowUpSLine size='2em' />
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default BackToTop;
