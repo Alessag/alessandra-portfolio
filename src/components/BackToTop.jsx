29367280ee5ab70fc985252e289a2d488f08f672
Alessandra <alessamica@gmail.com>
2020-10-18 01:06:50 -0400

    Include emotion core

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from '@emotion/styled';
+import { jsx } from '@emotion/core';
 import { css } from 'emotion';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import { RiArrowUpSLine } from 'react-icons/ri';
+
+const gray = '#bcbcbc';
+const purple = '#8083ff';
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
-
-const gray = '#bcbcbc';
-const purple = '#8083ff';
 
 const BackToTop = () => {
   return (
